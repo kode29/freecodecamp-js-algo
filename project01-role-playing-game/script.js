837e75c006fbe05338f5c6ee0f3fa9ab6e93548f
Kyle M. Perkins <kp@q5i.co>
2024-01-04 03:17:22 -0500

    step 169

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -254,7 +254,7 @@ function pick(guess){
         numbers.push(Math.floor(Math.random() * 11));
     }
     text.innerText = "You picked "+guess+". Here are the random numbers:\n";
-    for (let i=0; i<5; i++){
+    for (let i=0; i<10; i++){
 
     }
 }
