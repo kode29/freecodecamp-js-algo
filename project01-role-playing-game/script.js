ab052839de62702e3b9ef674b46d568746a04683
Kyle M. Perkins <kp@q5i.co>
2024-01-04 01:13:40 -0500

    step 78 done

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -65,10 +65,14 @@ function fightDragon(){
     console.log("Fighting dragon.");
 }
 function buyHealth(){
-    gold -= 10;
-    health += 10;
-    goldText.innerText = gold;
-    healthText.innerText = health;
+    if (gold >= 10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        
+    }
 }
 function buyWeapon(){
 
