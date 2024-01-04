87e9a3ba9bf391ef54414fd54b54bda4001dfefb
Kyle M. Perkins <kp@q5i.co>
2024-01-04 02:17:30 -0500

    step 128

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -177,10 +177,10 @@ function attack(){
     }
 }
 function dodge(){
-
+    text.innerText = "You dodge the attack from the "+monsters[fighting].name+".";
 }
 function defeatMonster(){
 }
 function lose(){
-    
+
 }
