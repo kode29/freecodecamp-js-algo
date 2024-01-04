88c04ef97a75870755b40abaf6aed1ed18432108
Kyle M. Perkins <kp@q5i.co>
2024-01-04 00:45:37 -0500

    step 65 done

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -37,8 +37,8 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location){
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
-    button1.innerText = "Go to store";
+    text.innerText = location["text"];
+    button1.innerText = location["button text"][0];
     button1.onclick = goStore;
     button2.innerText = "Go to cave";
     button2.onclick = goCave;
