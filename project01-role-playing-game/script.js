4f010a089d62254cb261b634635e5d8d7247ac03
Kyle M. Perkins <kp@q5i.co>
2024-01-04 02:32:37 -0500

    step 138

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -84,11 +84,11 @@ const locations = [
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
-        name: "kill monster",
-        "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
-        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
-    },
+        name: "lose",
+        "button text": ["REPLAY?","REPLAY?","REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You die. ☠️"
+    }
 ];
 
 
