525b33a3849b6172cdc6dc2c6728d41b3719633e
Kyle M. Perkins <kp@q5i.co>
2024-01-04 03:13:04 -0500

    step 164

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -251,7 +251,7 @@ function easterEgg(){
 function pick(guess){
     const numbers = [];
     while (numbers.length < 10){
-        
+        numbers.push(Math.floor(Math.random() * 11));
     }
 }
 function pickTwo(){
