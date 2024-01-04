80df7c9fa9b18c88d218e1688a552b7ce01846aa
Kyle M. Perkins <kp@q5i.co>
2024-01-04 01:15:22 -0500

    step 81 done

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -5,6 +5,19 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [];
 const locations = [
     {
         name: "town square",
@@ -26,16 +39,6 @@ const locations = [
     }
 ];
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
 
 // initialize buttons
 button1.onclick = goStore;
