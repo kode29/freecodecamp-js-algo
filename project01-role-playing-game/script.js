25fef98d1567e4774d942bebe11a935804d00b02
Kyle M. Perkins <kp@q5i.co>
2024-01-04 01:23:42 -0500

    step 90

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -99,8 +99,7 @@ function buyWeapon(){
         gold -= 30;
         goldText.innerText = gold;
         currentWeapon++;
-        let newWeapon = weapons[currentWeapon];
-
+        let newWeapon = weapons[currentWeapon].name;
         text.innerText = "You now have a new weapon.";  
 
     }
