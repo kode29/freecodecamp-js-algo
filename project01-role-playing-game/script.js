1fb5fe53bc48fa552ffb0325e548f8e1f81972cb
Kyle M. Perkins <kp@q5i.co>
2024-01-04 00:36:32 -0500

    step 52 done

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -5,6 +5,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+const locations = [];
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -23,7 +24,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location){
-    
+
 }
 
 function goTown(){
