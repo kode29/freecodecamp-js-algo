6051eae00620380036d3067c22676566f4c68994
Kyle M. Perkins <kp@q5i.co>
2024-01-04 02:09:14 -0500

    step 121

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -166,6 +166,7 @@ function goFight(){
 function attack(){
     text.innerText = "The "+monsters[fighting].name+" attacks.";
     text.innerText += " You attack it with your "+weapons[currentWeapon].name+".";
+    health -= monsters[fighting].level;
 }
 function dodge(){
 
