0e8dab7d8b86f74ec2e4526e77ea7d647bc1aa77
Kyle M. Perkins <kp@q5i.co>
2024-01-04 01:28:31 -0500

    step 97

--- project01-role-playing-game/script.js
+++ project01-role-playing-game/script.js
@@ -95,7 +95,7 @@ function buyHealth(){
     }
 }
 function buyWeapon(){
-        if (currentWeapon < 3) {
+    if (currentWeapon < weapons.length) {
         if (gold >= 30){
             gold -= 30;
             goldText.innerText = gold;
