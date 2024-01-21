8b12e355289b1e63be5e3ada80fd7c1f3dc13a99
Kyle M. Perkins <kp@q5i.co>
2024-01-21 01:04:59 -0500

    finish step 60

--- project03-music-player/script.js
+++ project03-music-player/script.js
@@ -177,7 +177,9 @@ const renderSongs = (array) => {
 };
 
 const setPlayButtonAccessibleText = () => {
-  const song =  userData?.currentSong || userData?.songs[0]
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute('aria-label', song?.title ? `Play ${song.title}` : "Play")
+  
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData.currentSong);
