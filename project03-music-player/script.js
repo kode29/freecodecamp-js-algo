9384837111fb7c2a8dce142a1b7f48516ff4ff59
Kyle M. Perkins <kp@q5i.co>
2024-01-21 01:05:40 -0500

    finish step 62

--- project03-music-player/script.js
+++ project03-music-player/script.js
@@ -122,7 +122,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () =>{
+const playPreviousSong = () => {
    if (userData?.currentSong === null) return;
    else {
     const currentSongIndex = getCurrentSongIndex();
@@ -132,7 +132,9 @@ const playPreviousSong = () =>{
    }
 };
 
+const shuffle = () => {
   
+};
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
