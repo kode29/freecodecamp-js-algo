2382bfff1b911248f8cfed3595abb98df6f19774
Kyle M. Perkins <kp@q5i.co>
2024-01-21 23:14:23 -0500

    finish step 75

--- project03-music-player/script.js
+++ project03-music-player/script.js
@@ -159,8 +159,9 @@ const deleteSong = (id) => {
 
   if (userData.songs.length === 0) {
     const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
   
-  
+
   }
 
 };
