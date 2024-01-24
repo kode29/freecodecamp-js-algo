69d2468bc8a4521851c7e09847f2566b88608d77
Kyle M. Perkins <kp@q5i.co>
2024-01-24 02:17:19 -0500

    finish

--- project05-palindrome-checker/script.js
+++ project05-palindrome-checker/script.js
@@ -1,14 +1,35 @@
 function palindrome(str) {
     // Good luck!
-    str = str.toLowerCase();
+    str = str.toLowerCase().replace(/[\W_]/g, "");
     
-    str = str.replace(/[\W_]/g, "");
     var reverse = str.split("").reverse().join("");
-    console.log(str + "\n"+reverse);
+    // console.log(str + "\n"+reverse);
     
-    if (str == reverse)
-      return true;
-    else
-      return false;
+    return (str == reverse) ? true : false;
   }
   
+const checkBtn =  document.getElementById("check-btn");
+const results = document.getElementById("result");
+const textInput = document.getElementById("text-input");
+
+const checkForPalindrome = () => {
+    const input = textInput.value;
+    if (input.length==0){
+        alert("Please input a value");
+        return;
+    } else {
+        results.innerHTML = `<strong>${input}</strong> ${palindrome(input) ? "is" : "is not"} a palindrome.`;;
+    }
+}
+
+checkBtn.addEventListener("click", () => {
+    checkForPalindrome()
+    textInput.value = '';
+})
+
+textInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkForPalindrome();
+    textInput.value = '';
+  }
+})
